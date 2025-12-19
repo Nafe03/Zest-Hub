edc0be116a42087a2e3982a5d76af6631adbd844
Nafe03 <rustpro29@gmail.com>
2025-12-19 18:29:15 +0100

    Update gui.lua

--- gui.lua
+++ gui.lua
@@ -886,7 +886,7 @@ function UILibrary.new(options)
                         SliderFrame.Name = id .. "Slider"
                         SliderFrame.Parent = GroupboxContent
                         SliderFrame.BackgroundTransparency = 1
-                        SliderFrame.Size = UDim2.new(1, 0, 0, 22)
+                        SliderFrame.Size = UDim2.new(1, 0, 0, 30)
                         SliderFrame.LayoutOrder = #self.Elements + 1
 
                         SliderText.Name = "Text"
